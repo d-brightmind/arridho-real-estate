54e98e3bab3bce4a8d237e5f6b7baed1e9ddb86e
d-brightmind <robiuridwanullah123@gmail.com>
2024-10-02 22:37:21 +0100

    Updating the Sign In UI

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -21,7 +21,7 @@ export default function SingIn() {
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
           <form>
-            <input className='w-full' type="email" id='email' value={email} onChange={onChange} placeholder='Email Address' className=""/>
+            <input className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out' type="email" id='email' value={email} onChange={onChange} placeholder='Email Address'/>
           </form>
         </div>
       </div>
